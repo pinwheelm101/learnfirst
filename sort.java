c5728d586943e07ba8aa9f823fea288aa1e47747
pinwheelm101 <923554614@qq.com>
2018-05-02 15:34:03 +0800

    最终

--- sort.java
+++ sort.java
@@ -6,8 +6,8 @@ public class BubbleSort {
 　　　　for(int num:arr){
 　　　　　　System.out.print(num+" ");
 　　　　}
-　　　　for(int i=0;i<arr.length-1;i++){//翌蚊儉桟陣崙電會緬方
-　　　　　　for(int j=0;j<arr.length-1-i;j++){//坪蚊儉桟陣崙耽匯緬電會謹富肝
+　　　　for(int i=0;i<arr.length-1;i++){
+　　　　　　for(int j=0;j<arr.length-1-i;j++){
 　　　　　　　　if(arr[j]>arr[j+1]){
 　　　　　　　　　　int temp=arr[j];
 　　　　　　　　　　arr[j]=arr[j+1];
