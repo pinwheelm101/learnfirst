3998f13445fdbddb892c54eba4f1d4f2918b2e22
pinwheelm101 <923554614@qq.com>
2018-05-05 14:34:55 +0800

    xx

--- laerngit/Rentcar/src/com/rentcar/main.java
+++ laerngit/Rentcar/src/com/rentcar/main.java
@@ -18,10 +18,10 @@ public class main {
      
     	 Scanner scanner= new Scanner(System.in);
     	 
-    	 System.out.printf("欢迎使用租车系统：%n@@@@是否需要租车： 1：是    2：否");
+    	 System.out.printf("欢迎使用租车系统：%n@@@@是否需要租车： 1：是    2：否%n");
     	 int in=scanner.nextInt();
     	 if(in==1) {
-    		 System.out.println("以下是车辆信息和价目表：/n");
+    		 System.out.println("以下是车辆信息和价目表：\n");
     		 System.out.println("序号\t名称\t租金\t载客载货量");
     		 for(int i=0,a=cars.length;i<a;i++) {
     			 System.out.printf("%d.\t%s\t\t%d元/天\t%s%n",i+1,cars[i].getName(),cars[i].getPrice(),cars[i].getType());
@@ -30,11 +30,11 @@ public class main {
     		 //此描述方式应该能适用于所有单类信息的操作；但利用这个数组，长度确定，只有元素值的变量，如果长度可以变化，
     		 //如果长度可以变化,设
     		 int im[]=new int [cars.length];
-    		     int affirm=0;   
-    		       do {
+    		      
+    		   int affirm=0;  
+    		    do {
     		        System.out.println("@@@@请输入需要租车类型(输入序号)：");
-    		    
-    		        int type=scanner.nextInt();
+    		    	int type=scanner.nextInt();
     		        if(type>cars.length+1) {
     		        	System.out.println("@@@请重新输入租车类型：");
     		        	type=scanner.nextInt();
@@ -42,9 +42,10 @@ public class main {
     		        System.out.println("@@@@请输入需要租车数量：");
     		        int num=scanner.nextInt();
     		        im[type-1]=num;//利用数组im记录租车数量，其下标同车型一一对应
-    		        System.out.print("@@@@是否确认输入：   1：确认   2：继续选择");
+    		        System.out.printf("@@@@是否确认输入：   1：确认   2：继续选择%n");
     		         affirm=scanner.nextInt();
-    	     }  while(affirm==1);    
+    		        
+    	        } while(affirm==2);    
     		        
     	            System.out.println("请输入租车天数：");
     		        int day=scanner.nextInt();
@@ -60,7 +61,7 @@ public class main {
     		        		passengerNum+=((Ipassenger) cars[i]).getPassengerNum()*im[i];//通过接口使父类对象引用子类方法
     		        	} 
     		        }
-    		        System.out.printf("共载人：%d人%n",passengerNum);
+    		        System.out.printf("共载人：  %d人%n",passengerNum);
     		        
     		        System.out.println("——————可载货的车有：");
     		        int cargoNum=0;
@@ -76,14 +77,14 @@ public class main {
     		        int priceSum=0;//找出价格属性，遍历求和
     		        for(int i=0,a=im.length;i<a;i++) {
     		        	if(im[i]!=0) {
-    		        		priceSum+=cars[i].getPrice()*im[i];
+    		        		priceSum+=cars[i].getPrice()*im[i]*day;
     		        	}
     		        }
     		        System.out.printf("——————租车总价%d元%n",priceSum);	
     		        System.out.println("感谢你的使用！！");
-    		    }
+    		    }else {System.out.println("感谢你的光临！！");}
     			 
-    		// }else {System.out.println("感谢你的光临！！");}
+    		// }
     	        scanner.close();
      }
 }
